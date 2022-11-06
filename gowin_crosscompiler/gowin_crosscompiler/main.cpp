a2b43472c8921ad03447bddf99cc4dbd0e37d5b0
Nagai Akina <aaaaakina0802@gmail.com>
2022-11-06 16:33:40 +0900

    新規プロジェクト追加
    中身は空

---
+++ gowin_crosscompiler/gowin_crosscompiler/main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#define IN_FILENAME  "./source.asm"
+#define OUT_FILENAME "./rom_init.mif"
+
+/* OP Codes */
+#define NOP 0
+#define ADD 1
+#define SUB 2
+#define AND 3
+#define OR  4
+#define SL  5
+#define SR  6
+#define SRA 7
+#define LDL 8
+#define LDH 9
+#define CMP 10
+#define JE  11
+#define JMP 12
+#define LD  13
+#define ST  14
+#define MOV 15
+
+int main() {
+
+}
